92776e4eb8f497d4a69dddb4159bd29aa86d50c3
Andrew Michael Calhoun <andrew.m.calhoun@gmail.com>
2018-12-16 23:40:18 -0800

    Update dbconnection.js

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -1,18 +1,1 @@
-var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'vinlydemo'
-  //insecureAuth: true
-});
-
-connection.connect();
-
-/* connection.connect(function (err) {
-  if (err) throw err;
-});
- */
-module.exports = connection;
+//Haha. No.
